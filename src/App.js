11ff9e6611d0e6bae417b1dbeeb2c9eff31239ee
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-09 00:08:45 -0800

    added full addder and substracter functionality using usestate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import { useState } from "react"
+
 
 function App() {
+// her setNumber is a function
+    const [number,setNumber] = useState(0)
 
+const addNumber=()=>{
+    setNumber(number+1);
+   
 
-function addNumber() {   }
+}
 
-function substractNumber() {}
+const substractNumber=()=>{
+  setNumber(number-1);
+
+}
 
   return (
     <div className='container'> 
    <ul>
     <li><button onClick={addNumber}>Add</button></li>
-    <li>Number</li>
+    <li>{number}</li>
     <li><button onClick={substractNumber}>Substract</button></li>
    </ul>
     </div>
