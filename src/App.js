8dd98f1e86eb77ff94fcb77cf7b492e351c5589a
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-08 23:32:52 -0800

    add styling and css

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container'> 
+   <ul>
+    <li><button>Add</button></li>
+    <li>Number</li>
+    <li><button>Substract</button></li>
+   </ul>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
 
