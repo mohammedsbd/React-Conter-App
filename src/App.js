9c133a4f9b2b9a29de64ad87d4351cfe4d99b672
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-08 23:37:43 -0800

    added the two function to add and substract

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 function App() {
+
+
+function addNumber() {   }
+
+function substractNumber() {}
+
   return (
     <div className='container'> 
    <ul>
-    <li><button>Add</button></li>
+    <li><button onClick={addNumber}>Add</button></li>
     <li>Number</li>
-    <li><button>Substract</button></li>
+    <li><button onClick={substractNumber}>Substract</button></li>
    </ul>
     </div>
   )
